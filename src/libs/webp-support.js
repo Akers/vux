675a399e667809e3cf17e6883bcb267296d23e1c
Akers Liang <akersliang@gmail.com>
2019-08-15 15:01:00 +0800

    fix: 修复IOS10使用的the operation is insecure报错
    修复libs下的webp-support.js，如果在判断localStorage为空时抛出异常，视为不支持localStorage处理

--- src/libs/webp-support.js
+++ src/libs/webp-support.js
@@ -2,7 +2,11 @@ const WEBP_NAME = 'can_use_webp'
 
 export function detectWebp () {
   if (typeof window === 'undefined') return
-  if (!window.localStorage || typeof localStorage !== 'object') return
+  try {
+    if (!window.localStorage || typeof localStorage !== 'object') return
+  } catch {
+    return
+  }
   if (!localStorage.getItem(WEBP_NAME) || (localStorage.getItem(WEBP_NAME) !==
       'available' && localStorage.getItem(WEBP_NAME) !== 'disable')) {
     var img = document.createElement('img')
